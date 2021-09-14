a8a5b4d125be86c9e618d66cac3dd56b1143b181
Karim Ayman <nighthawk1486@gmail.com>
2021-09-14 23:29:41 +0200

    Converting the App from class to functional component

--- src/App.js
+++ src/App.js
@@ -6,41 +6,29 @@ import Shelf from './Shelf';
 import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Route path='/search' render={() => (
-          <Search />
-        )} />
-        <Route exact path='/' render={() => (
-          <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <Shelf />
-            </div>
-          </div>
-          <div className="open-search">
-            <Link className="open-search-link" to='/search'>Add a book</Link>
+const BooksApp = () => {
+  return (
+    <div className="app">
+      <Route path='/search' render={() => (
+        <Search />
+      )} />
+      <Route exact path='/' render={() => (
+        <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Shelf />
           </div>
         </div>
-        )} />
+        <div className="open-search">
+          <Link className="open-search-link" to='/search'>Add a book</Link>
+        </div>
       </div>
-    )
-  }
+      )} />
+    </div>
+  )
 }
 
 export default BooksApp
